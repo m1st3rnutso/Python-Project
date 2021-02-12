139a83d0668fd410ac07400e3e1d39f4bb94a826
George <georgemichaelcallan@gmail.com>
2021-02-12 18:33:48 +1000

    Learning Loops

---
+++ Python Project.py
@@ -0,0 +1,13 @@
+def printSomething(word):
+    print(word)
+
+def addTwoNumbers(x, y):
+    print(x)
+    print(y)
+    print(x + y)
+
+def printMultipleTimes (word, numberOfTimesToPrint) :
+    for i in range (0, numberOfTimesToPrint) :
+        print(str(i) + " word")
+
+printMultipleTimes("hello", 5)
