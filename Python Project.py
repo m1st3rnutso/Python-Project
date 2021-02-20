22f11d25ce90f342f67ddd4cf97062ac56b5238e
George <georgemichaelcallan@gmail.com>
2021-02-20 18:14:57 +1000

    Learning data types and classes

--- Python Project.py
+++ Python Project.py
@@ -8,6 +8,26 @@ def addTwoNumbers(x, y):
 
 def printMultipleTimes (word, numberOfTimesToPrint) :
     for i in range (0, numberOfTimesToPrint) :
-        print(str(i) + " word")
+        print(str(i) + " " + word)
 
-printMultipleTimes("hello", 5)
+class Dog:
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def changeName(self, newName):
+        self.name = newName
+    
+
+dogOne = Dog("chilla", "borderCollie", 15)
+dogTwo = Dog("charlie", "kelpie", 7)
+
+print(dogOne.name)
+
+dogOne.changeName("bob")
+
+print(dogOne.name)
+
+# challenge: make a new class: Car
+# which is constructed with properties: make, model, year
